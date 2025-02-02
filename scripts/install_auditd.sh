c4678a0f7944fb461b1ed989fe494cb461f9420f
Hari Sekhon <harisekhon@gmail.com>
2025-02-03 02:38:27 +0700

    added install_auditd.sh

---
+++ scripts/install_auditd.sh
@@ -0,0 +1,29 @@
+#!/usr/bin/env bash
+#  vim:ts=4:sts=4:sw=4:et
+#
+#  Author: Hari Sekhon
+#  Date: 2025-02-03 01:16:52 +0700 (Mon, 03 Feb 2025)
+#
+#  https///github.com/HariSekhon/Packer
+#
+#  License: see accompanying Hari Sekhon LICENSE file
+#
+#  If you're using my code you're welcome to connect with me on LinkedIn and optionally send me feedback to help steer this or other code I publish
+#
+#  https://www.linkedin.com/in/HariSekhon
+#
+
+set -euo pipefail
+[ -n "${DEBUG:-}" ] && set -x
+
+timestamp "Installing Auditd"
+echo
+# shellcheck disable=SC2154
+rpm -q audit ||
+$sudo yum install audit
+echo
+$sudo chkconfig auditd on ||
+$sudo systemctl enable auditd
+echo
+$sudo service auditd start ||
+$sudo systemctl start auditd
